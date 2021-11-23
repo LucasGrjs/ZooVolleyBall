df20546d971e0c8284dd96c2a211a501db4f4d4e
NiiRoZz <extazziii1000@gmail.com>
2021-11-23 10:20:59 +0100

    Add default

---
+++ src/main/java/project/Application.java
@@ -0,0 +1,20 @@
+package project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication(scanBasePackages= {"project"})
+@EnableTransactionManagement
+@ImportResource(value = "classpath:spring/application-config.xml")
+public class Application {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+		
+		
+	}
+	
+
+}
+ 
