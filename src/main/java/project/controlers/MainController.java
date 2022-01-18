2a9764d0a70a4786c6510e645b097b8779e5f935
ldacunha <Lucas.dacunha.pro@gmail.com>
2022-01-18 15:09:39 +0100

    STOP SUPPRIMER VIE DE MA MERE

--- src/main/java/project/controlers/MainController.java
+++ src/main/java/project/controlers/MainController.java
@@ -1,5 +1,4 @@
 package project.controlers;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,9 +14,8 @@ import project.repositories.DemandeAmiRepository;
 import project.repositories.DemandePartieRepository;
 import project.repositories.UsersRepository;
 import project.services.IDemandeAmiManagement;
-
+import project.services.IDemandePartieManagement;
 import java.util.List;
-
 @Controller
 public class MainController {
 
@@ -28,15 +26,33 @@ public class MainController {
     private IDemandeAmiManagement demandeAmiManagement;
 
     @Autowired
+    private IDemandePartieManagement demandePartieManagement;
+
+    @Autowired
     DemandeAmiRepository demandeAmiRep;
 
     @Autowired
     DemandePartieRepository demandePartieRep;
 
-    @RequestMapping("main")
-    public String mainPage(@RequestParam(value="friend", required=false) String pseudo,
-                           @RequestParam(value="demandeur", required = false) String demandeur, Model model)
-    {
+    @RequestMapping("demandeur")
+    public String demandeur(@RequestParam(value="demandeur", required = true) String demandeur, Model model) {
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        User user = usersRepository.findByEmail(userDetails.getUsername());
+
+        User dem = usersRepository.findByPseudo(demandeur);
+        demandeAmiManagement.removeDemandeAmi(dem, user);
+        demandeAmiManagement.removeDemandeAmi(user, dem);
+        user.getAmis().add(dem);
+        dem.getAmis().add(user);
+        usersRepository.save(user);
+        usersRepository.save(dem);
+
+        return "redirect:/main";
+    }
+
+    @RequestMapping("friend")
+    public String friend(@RequestParam(value="friend", required=true) String pseudo, Model model) {
         UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
         User user = usersRepository.findByEmail(userDetails.getUsername());
@@ -45,16 +61,28 @@ public class MainController {
                 !user.getAmis().contains(usersRepository.findByPseudo(pseudo))) {
             demandeAmiManagement.addDemandeAmi(new DemandeAmi(user, usersRepository.findByPseudo(pseudo)));
         }
+        return "redirect:/main";
+    }
 
-        if (demandeur != null) {
-            User dem = usersRepository.findByPseudo(demandeur);
-            demandeAmiManagement.removeDemandeAmi(dem, user);
-            demandeAmiManagement.removeDemandeAmi(user, dem);
-            user.getAmis().add(dem);
-            dem.getAmis().add(user);
-            usersRepository.save(user);
-            usersRepository.save(dem);
-        }
+    @RequestMapping("demandeurPartie")
+    public String demandeurPartie(@RequestParam(value = "demandeurPartie", required = true) String demandeurPartie,
+                                  Model model) {
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        User user = usersRepository.findByEmail(userDetails.getUsername());
+
+        User dem = usersRepository.findByPseudo(demandeurPartie);
+        demandePartieManagement.removeDemandePartie(dem, user);
+        model.addAttribute("idUser",user.getIdUser());
+        return "game";
+    }
+
+    @RequestMapping("main")
+    public String mainPage(Model model)
+    {
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        User user = usersRepository.findByEmail(userDetails.getUsername());
 
         model.addAttribute("User",user);
         if(!(user.getNbrWin() + user.getNbrLoss() == 0))
@@ -63,13 +91,10 @@ public class MainController {
         }else{
             model.addAttribute("Winrate", "You need to play match");
         }
-
         List<DemandeAmi> demandes = demandeAmiRep.findByReceveur(user);
         model.addAttribute("Demandes", demandes);
-
         List<DemandePartie> demandesPartie = demandePartieRep.findByReceveur(user);
         model.addAttribute("DemandesPartie", demandesPartie);
-
         return "main";
     }
 
--- src/main/java/project/services/IDemandePartieManagement.java
+++ src/main/java/project/services/IDemandePartieManagement.java
@@ -1,7 +1,9 @@
 package project.services;
 
 import project.model.DemandePartie;
+import project.model.User;
 
 public interface IDemandePartieManagement {
     DemandePartie addDemandePartie(DemandePartie demandePartie);
+    void removeDemandePartie(User receveur, User demandeur);
 }
